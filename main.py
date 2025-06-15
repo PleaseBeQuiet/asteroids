f5c50bc1925106ca2321aa490e54f63bb4bcee21
PleaseBeQuiet <127631362+PleaseBeQuiet@users.noreply.github.com>
2025-06-15 18:29:08 -0400

    add sircleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +17,7 @@ def main():
 
         screen.fill(0)
         pygame.display.flip()
+        dt = (game_clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
